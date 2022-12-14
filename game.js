c6291e764be9fbab8a726d212bf04f9f794803f2
Zeladd <filippesteam@gmail.com>
2022-12-15 01:46:34 +0200

    Finish the js part and change the if statement with a switch case

--- game.js
+++ game.js
@@ -1,11 +1,4 @@
 const variants = ["rock","paper","scissors"];
-var computerScore = 0;
-var playerScore = 0;
-
-function setScore(playerScore, computerScore) {
-    let score = toString(playerScore) + " - " + toString(computerScore) + " (player - computer)";
-    return score
-}
 
 function getComputerChoice() {
     let choise = Math.floor(Math.random() * 3)
@@ -14,58 +7,60 @@ function getComputerChoice() {
 
 function playerSelection() {
     let choise = prompt("Choose rock/paper/scissors");
+    while (variants.includes(choise) === false) {
+        choise = prompt("Choose rock/paper/scissors");
+    }
     return choise.toLowerCase();
 }
 
-function playRound(playerChoice, computerChoice) {
-    var win;
-
-    if (playerChoice == computerChoice) {
-        console.log("It's a draw!")
-    } else if (playerChoice == "rock" && computerChoice == "paper") {
-        console.log("You lose!")
-        win = false;
-    } else if(playerChoice == "rock" && computerChoice == "scissors") {
-        console.log("You win!")
-        win = true;
-    } else if (playerChoice == "paper" && computerChoice == "rock") {
-        console.log("You win!")
-        win = true;
-    } else if(playerChoice == "paper" && computerChoice == "scissors") {
-        console.log("You lose!")
-        win = false;
-    } else if (playerChoice == "scissors" && computerChoice == "rock") {
-        console.log("You lose!")
-        win = false;
-    } else if(playerChoice == "scissors" && computerChoice == "paper") {
-        console.log("You win!")
-        win = true;
-    } else {
-        console.log("Please choose between rock/paper/scissors!");
+function playRound(playerSelection, computerChoice) {
+    if(playerSelection === computerChoice) {
+        return "It's a draw!";
     }
-
-    if (win == true) {
-        playerScore += 1;
-        console.log(win);
-        console.log(setScore(playerScore , computerScore));
-        console.log(playerScore);
-    } else if (win == "false") {
-        computerScore += 1;
-        console.log(win);
-        console.log(setScore(playerScore , computerScore));
+    else {
+        switch (playerSelection) {
+            case "rock":
+                if (computerChoice === "scissors") {
+                    return "You win, rock beats scissors!";
+                }
+                else {
+                    return "You lose, paper beats rock!";
+                }
+                break;
+            case "paper":
+                if (computerChoice === "rock") {
+                    return "You win, paper beats rock!";
+                }
+                else {
+                    return "You lose, scissors beats paper!";
+                }
+                break;
+            case "scissors":
+                if (computerChoice === "paper") {
+                    return "You win, scissors beats paper!";
+                }
+                else {
+                    return "You lose, rock beats scissors!";
+                }
+                break;
+        }
     }
 }
-
 function game() {
+    let pScore = 0;
+    let cScore = 0;
     console.log("Welcome to rock/paper/scissors game (best of 5)");
-    while (playerScore < 3 || computerScore < 3) {
-        playRound(playerSelection(), getComputerChoice());
-        console.log(score);
-    }
-    if (playerScore == 3) {
-        console.log("Congrats! You won the game");
-    } else {
-        console.log("The computer won the game :(");
+    console.log(pScore.toString() + " - " + cScore.toString() + " (Player - Computer)");
+    for(let i = 0; i < 5; i++) {
+        let winningStatement = playRound(playerSelection(), getComputerChoice());
+        if (winningStatement.includes("You win")) {
+            pScore += 1; 
+        }
+        else if (winningStatement.includes("You lose")) {
+            cScore +=1;
+        }
+        console.log(winningStatement);
+        console.log(pScore.toString() + " - " + cScore.toString() + " (Player - Computer)");
     }
 }
 
